9c0f20aa4571a8e63b9353834e7365d3c5f8de7b
megantriplett <megantriplett10@gmail.com>
2025-04-01 23:55:01 -0400

    added chat client from https://cs.lmu.edu/~ray/notes/javanetexamples/#chat in an attempt to test - did not work also added file reading functionality to P2PServer.js

--- P2PServer.js
+++ P2PServer.js
@@ -36,9 +36,27 @@ net
     socket.write('Welcome to the chat! Please enter your name: \n')
     socket.on("data", (buffer) =>{
         const message = buffer.toString("utf-8").trim()
+        fileName = "." + fileName;
+		
+
+      
+    const fileSelector = document.getElementById('file-selector');
+    fileSelector.addEventListener('change', (event) => {
+        const fileList = event.target.files;
+        console.log(fileList);
+    });
+		// Open the requested file.
+		// FileInputStream fis = null;
+		// let fileExists = true;
+		// try { fis = new FileInputStream(fileName);
+		// } catch (error) {
+		//     fileExists = false;
+		// }
+
         if(!name){
             name = tryAcceptName(socket, message)
         } else {
+            const {file, name, type, size} = getMetadataForFileList(fileList)
             broadcastClientMessage(socket, '${name}: ${message}')
         }
     })
@@ -46,6 +64,17 @@ net
 }
 )
 
+function getMetadataForFileList(fileList) {
+    for (const file of fileList) {
+      // Not supported in Safari for iOS.
+      const name = file.name ? file.name : 'NOT SUPPORTED';
+      // Not supported in Firefox for Android or Opera for Android.
+      const type = file.type ? file.type : 'NOT SUPPORTED';
+      // Unknown cross-browser support.
+      const size = file.size ? file.size : 'NOT SUPPORTED';
+      return ({file, name, type, size});
+    }
+  }
 
 server.listen(59090, () => {
     console.log("Running")
