b0dcb133cd963728ced2968fc9ad28a981ba8357
Shahir Ahmed <ahmeds@dickinson.edu>
2025-04-06 06:26:49 -0400

    feat: migrate from Vue to React, update project structure and add initial pages

--- api/controllers/messageController.js
+++ api/controllers/messageController.js
@@ -1,51 +1,57 @@
-import Message from "../models/Message.js"
+import Message from "../models/Message.js";
 
-export const sendMessage = async(req, res) => {
-    try {
-        const {content, receiverId} = req.body
+export const sendMessage = async (req, res) => {
+	try {
+		const { content, receiverId } = req.body;
 
-        const newMessage = await Message.create({
-            sender: req.user.id,
-            receiver: receiverId,
-            content
+		const newMessage = await Message.create({
+			sender: req.user.id,
+			receiver: receiverId,
+			content,
 
+			//SEND THE MESSAGE IN REAL TIME - later
+		});
+		res.status(201).json({
+			success: true,
+			message: newMessage,
+		});
+	} catch (error) {
+		console.log("Error in sendMessage: ", error);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+};
 
-            //SEND THE MESSAGE IN REAL TIME - later
-        })
-        res.status(201).json({
-            success: true,
-            message: newMessage
-        })
-    } catch (error) {
-        console.log("Error in sendMessage: ", error);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
-    }
+export const getConversation = async (req, res) => {
+	const { userId } = req.params;
 
-}
+	try {
+		const messages = await Message.find({
+			// find messages sent by us OR received by us
+			$or: [
+				{ sender: req.user._id, receiver: userId }, // messages sent by us
+				{ sender: userId, receiver: req.user._id }, // messages received by us
+			],
+		}).sort("createdAt"); // see the latest messages first and the oldest messages last
 
-export const getConversation = async(req, res) => {
-    const { userId } = req.params;
-    try {
-        const messages = await Message.find({
-            $or: [
-                {sender: req.user._id, receiver: userId},
-                {sender: userId, receiver: req.user._id}
-            ]
-        }).sort("createdAt")
+		if (!messages) {
+			return res.status(404).json({
+				success: false,
+				message: "No messages found",
+			});
+		}
 
-        res.status(200).json({
-            success: true,
-            messages
-        })
-        
-    } catch (error) {
-        console.log("Error in getConversation: ", error);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
-    }
-}
+		res.status(200).json({
+			success: true,
+			messages,
+		});
+	} catch (error) {
+		console.log("Error in getConversation: ", error);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+};
